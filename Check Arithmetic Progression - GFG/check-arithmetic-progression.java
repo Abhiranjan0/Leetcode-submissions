77a54bdf4afa4c6b26f3b98f78e73beb1a903e39
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-05-23 19:17:01 +0530

    Added solution - LeetHub

---
+++ Check Arithmetic Progression - GFG/check-arithmetic-progression.java
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+
+
+class Solution
+{
+    boolean checkIsAP(int arr[] ,int n)
+    {
+
+        Arrays.sort(arr);
+        int k = (arr[0]-arr[1]);
+        
+        for(int i=1; i<arr.length-1; i++) {
+            if(k!= arr[i]-arr[i+1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
+// { Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main (String[] args) throws IOException{
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while(testcases-- > 0){
+            int n=Integer.parseInt(br.readLine());
+
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int a[]=new int[n];
+            for(int  i=0;i<n;i++)
+            {
+                a[i]=Integer.parseInt(a1[i]);
+            }
+            Solution ob=new Solution();
+
+            boolean ans=ob.checkIsAP(a,n);
+            if(ans==true)
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
+
+
+
+  // } Driver Code Ends
