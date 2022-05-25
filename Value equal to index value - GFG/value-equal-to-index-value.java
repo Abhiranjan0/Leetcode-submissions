fbdbe9f1b29b830a9e2a631dfe8fd767dcfc88bd
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-05-25 23:31:51 +0530

    Added solution - LeetHub

---
+++ Value equal to index value - GFG/value-equal-to-index-value.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            ArrayList<Integer> ans = new Solution().valueEqualToIndex(arr, n);
+            if (ans.size() == 0) {
+                System.out.println("Not Found");
+            } else {
+                for (Integer x : ans) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        
+       ArrayList<Integer> ans = new ArrayList<>();
+       for(int i=0; i<arr.length; i++) {
+           int j = i+1;
+           if(arr[i] == j)
+             ans.add(j);
+       }
+       return ans;
+    }
+}
