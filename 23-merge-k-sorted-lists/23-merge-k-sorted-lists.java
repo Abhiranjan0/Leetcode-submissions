b7633f0e3324fed4e3d13e5e1ba9a3439c58c0b3
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-05-27 18:06:58 +0530

    Time: 2 ms (97.08%), Space: 47.8 MB (27.76%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,34 +10,44 @@
  */
 class Solution {
     
-    public int minNode(ListNode[] lists) {
-        int min  = Integer.MAX_VALUE;
-        int idx = -1;
+    ListNode merge2List(ListNode head1, ListNode head2) {
+       
+       ListNode dummy = new ListNode(-1);
+       ListNode head = dummy, tail = dummy;
+       while(head1 != null && head2 != null) {
+           if(head1.val < head2.val) {
+               tail.next = head1;
+               head1 = head1.next;
+           }
+           
+           else {
+               tail.next = head2;
+               head2 = head2.next;
+           }
+           tail = tail.next;
+       }
+       if(head1 != null) tail.next = head1;
+       else tail.next = head2;
+       return dummy.next;
+   } 
+    
+    public ListNode helper(ListNode[] lists, int left, int right) {
+        if(left>right) return null;
+        if(left == right) return lists[left];
         
-        for(int i=0; i<lists.length; i++) {
-            if(lists[i] != null && lists[i].val < min) {
-            idx = i;
-            min = lists[i].val;
-        }
-            
-      }
-        return idx;
+        int mid = (left+right)/2;
+        ListNode l1 = helper(lists, left, mid);
+        ListNode l2 = helper(lists, mid+1, right);
+        return merge2List(l1,l2);
     }
+    
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length == 0) {
             // 0 linked list
             return null;
         }
-        ListNode dummy = new ListNode(-1);
-        ListNode head = dummy, tail = dummy;
         
-        while(true) {
-            int minIdx = minNode(lists);
-            if(minIdx == -1)  break; 
-            tail.next = lists[minIdx];
-            lists[minIdx] = lists[minIdx].next;
-            tail = tail.next;
-        }
-        return dummy.next;
+        return helper(lists, 0, lists.length-1);
+       
     }
 }
