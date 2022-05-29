ed5586606390bbd9569773c494c9701762dfa41f
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-05-29 21:00:56 +0530

    Time: 2 ms (46.70%), Space: 55.7 MB (28.73%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,32 +11,35 @@
  */
 public class Solution {
     
-    public int getSize(ListNode head){
-        int size = 0;
-        while(head != null) {
-            head = head.next;
-            size++;
+     public ListNode detectCycle(ListNode head) {
+     ListNode slow = head, fast = head;
+        
+     while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast) break;
+     }
+        if(fast == null || fast.next == null)  // no Cycle
+            return null;
+        
+        ListNode curr1 = head, curr2 = slow;
+        while(curr1 != curr2) {
+            curr1 = curr1.next;
+            curr2 = curr2.next;
         }
-        return size;
-    }
+        return curr1;
+    }  
     
     
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int sizeA = getSize(headA);
-        int sizeB = getSize(headB);
-        
-        if(sizeA > sizeB) {
-            for(int i=0; i < sizeA-sizeB; i++) 
-                headA = headA.next;
-        }
-        else {
-            for(int i=0; i < sizeB-sizeA; i++)
-                headB = headB.next;
-        }
-        while(headA != headB) {
-            headA = headA.next;
-            headB = headB.next;
-        }
-        return headA;
+     ListNode tailB = headB;
+     while(tailB.next != null) {
+         tailB = tailB.next;
+     }  
+        tailB.next = headB;
+        ListNode ans = detectCycle(headA);
+        tailB.next = null;
+        return ans;
     }
 }
