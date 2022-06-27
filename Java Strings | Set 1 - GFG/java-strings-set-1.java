8aab72f9fa8e7ad98cdd6040fc7d8b4db6b57dc2
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-06-27 22:43:40 +0530

    Added solution - LeetHub

---
+++ Java Strings | Set 1 - GFG/java-strings-set-1.java
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S1 = read.readLine();
+            String S2 = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.conRevstr(S1, S2));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for Java
+
+class Solution {
+    static String conRevstr(String S1, String S2) {
+        // code here
+        S1 = S1 + S2;
+        S2 = "";
+        
+        for(int i=S1.length()-1; i>=0; i--) {
+            S2 = S2+ S1.charAt(i);
+        }
+        return S2;
+    }
+}
