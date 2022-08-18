7d2c83ffc4d2affdafbd9ca72ccade99b6897b63
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-08-18 21:26:54 +0530

    Time: 15 ms (18.57%), Space: 48.5 MB (17.04%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -1,16 +1,27 @@
 class Solution {
     public boolean divideArray(int[] nums) {
-       int pairs = nums.length/2;
-       Arrays.sort(nums);
-       int count = 0; 
+//        int pairs = nums.length/2;
+//        Arrays.sort(nums);
+//        int count = 0; 
        
-       for(int i=1; i<nums.length; i+=2) {
-           if(nums[i] == nums[i-1])
-               count ++;
+//        for(int i=1; i<nums.length; i+=2) {
+//            if(nums[i] == nums[i-1])
+//                count ++;
            
-       } 
-         if(pairs == count) return true;
-           else
-               return false;
+//       } 
+//          if(pairs == count) return true;
+//            else
+//                return false;
+        
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int value : nums) {
+            
+            map.put(value, map.getOrDefault(value,0)+1);
+        }
+        
+        for(Integer i : map.keySet()) {
+            if(map.get(i)%2 != 0) return false;
+        }
+        return true;
     }
 }
