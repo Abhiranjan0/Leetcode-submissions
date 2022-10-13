af3444e61b0f8d9ac24a1a57dcb6be7769462509
Vivek Yadav <94182479+VIVEK1918YADAV@users.noreply.github.com>
2022-10-13 22:27:27 +0530

    Added solution - LeetHub

--- Max Min - GFG/max-min.java
+++ Max Min - GFG/max-min.java
@@ -40,8 +40,23 @@ class Solution
     public static int findSum(int A[],int N) 
     {
         //code here
-        Arrays.sort(A);
-        int sum = A[0] + A[A.length-1];
-        return sum;
+        
+        //Approach 1
+        
+        // Arrays.sort(A);
+        // int sum = A[0] + A[A.length-1];
+        // return sum;
+        
+        //Approach 2
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<N; i++){
+            if(A[i] > max)
+            max = A[i];
+            
+            if(A[i] < min)
+            min = A[i];
+        }
+        return max+min;
     }
 }
